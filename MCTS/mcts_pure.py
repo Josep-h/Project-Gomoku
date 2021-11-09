2a7c4a3dfb1b57c1a3a95a78bda6d1195815c2fa
Josep-h <2870952487@qq.com>
2021-11-10 00:00:48 +0800

    MCTS

--- MCTS/mcts_pure.py
+++ MCTS/mcts_pure.py
@@ -21,7 +21,7 @@ def policy_value_fn(board):
     """a function that takes in a state and outputs a list of (action, probability)
     tuples and a score for the state"""
     # return uniform probabilities and 0 score for pure MCTS
-    action_probs = np.ones(len(board.availables))/len(board.availables)
+    action_probs = np.ones(len(board.availables)) / len(board.availables)
     return zip(board.availables, action_probs), 0
 
 
@@ -33,10 +33,10 @@ class TreeNode(object):
     def __init__(self, parent, prior_p):
         self._parent = parent
         self._children = {}  # a map from action to TreeNode
-        self._n_visits = 0 #访问次数
-        self._Q = 0 # 综合平均动作估值
-        self._u = 0 #exploration值
-        self._P = prior_p # 策略网络的先验概率
+        self._n_visits = 0  # 访问次数
+        self._Q = 0  # 综合平均动作估值
+        self._u = 0  # exploration值
+        self._P = prior_p  # 策略网络的先验概率
 
     def expand(self, action_priors):
         """Expand tree by creating new children.
@@ -52,8 +52,7 @@ class TreeNode(object):
         plus bonus u(P).
         Return: A tuple of (action, next_node)
         """
-        return max(self._children.items(),
-                   key=lambda act_node: act_node[1].get_value(c_puct))
+        return max(self._children.items(), key=lambda act_node: act_node[1].get_value(c_puct))
 
     def update(self, leaf_value):
         """Update node values from leaf evaluation.
@@ -63,11 +62,10 @@ class TreeNode(object):
         # Count visit.
         self._n_visits += 1
         # Update Q, a running average of values for all visits.
-        self._Q += 1.0*(leaf_value - self._Q) / self._n_visits
+        self._Q += 1.0 * (leaf_value - self._Q) / self._n_visits
 
     def update_recursive(self, leaf_value):
-        """Like a call to update(), but applied recursively for all ancestors.
-        """
+        """Like a call to update(), but applied recursively for all ancestors."""
         # If it is not root, this node's parent should be updated first.
         if self._parent:
             self._parent.update_recursive(-leaf_value)
@@ -80,13 +78,11 @@ class TreeNode(object):
         c_puct: a number in (0, inf) controlling the relative impact of
             value Q, and prior probability P, on this node's score.
         """
-        self._u = (c_puct * self._P *
-                   np.sqrt(self._parent._n_visits) / (1 + self._n_visits))
+        self._u = c_puct * self._P * np.sqrt(self._parent._n_visits) / (1 + self._n_visits)
         return self._Q + self._u
 
     def is_leaf(self):
-        """Check if leaf node (i.e. no nodes below this have been expanded).
-        """
+        """Check if leaf node (i.e. no nodes below this have been expanded)."""
         return self._children == {}
 
     def is_root(self):
@@ -117,7 +113,7 @@ class MCTS(object):
         State is modified in-place, so a copy must be provided.
         """
         node = self._root
-        while(1):
+        while 1:
             if node.is_leaf():
 
                 break
@@ -165,8 +161,7 @@ class MCTS(object):
         for n in range(self._n_playout):
             state_copy = copy.deepcopy(state)
             self._playout(state_copy)
-        return max(self._root._children.items(),
-                   key=lambda act_node: act_node[1]._n_visits)[0]
+        return max(self._root._children.items(), key=lambda act_node: act_node[1]._n_visits)[0]
 
     def update_with_move(self, last_move):
         """Step forward in the tree, keeping everything we already know
@@ -185,6 +180,7 @@ class MCTS(object):
 
 class MCTSPlayer(object):
     """AI player based on MCTS"""
+
     def __init__(self, c_puct=5, n_playout=2000):
         self.mcts = MCTS(policy_value_fn, c_puct, n_playout)
 
@@ -199,7 +195,7 @@ class MCTSPlayer(object):
         if len(sensible_moves) > 0:
             move = self.mcts.get_move(board)
             self.mcts.update_with_move(-1)
-            return move
+            return move, 0
         else:
             print("WARNING: the board is full")
 
